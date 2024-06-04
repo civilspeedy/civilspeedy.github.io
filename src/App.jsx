cb1692c05849da40d68fd99f02c6de2f43341b89
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-06-04 18:39:51 +0100

    moved icons

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,19 @@ import AboutMePage from './Components/Pages/AboutMePage';
 import { Box, Grow } from '@mui/material';
 import MenuButton from './Components/Buttons/MenuButton';
 
-import javaIcon from './assets/Java.svg';
-import pythonIcon from './assets/Python.svg';
-import javaScriptIcon from './assets/JavaScript.svg';
-import reactIcon from './assets/React.svg';
-import flaskIcon from './assets/Flask.svg';
-import sqliteIcon from './assets/Sqlite.svg';
-import nodeIcon from './assets/Node.svg';
-import HTMLIcon from './assets/HTML.svg';
-import tkinterIcon from './assets/Tkinter.png';
-import expoIcon from './assets/Expo.svg';
-import SQLIcon from './assets/SQL.svg';
-import bunIcon from './assets/Bun.svg';
-import viteIcon from './assets/Vite.svg';
+import javaIcon from './assets/icons/Java.svg';
+import pythonIcon from './assets/icons/Python.svg';
+import javaScriptIcon from './assets/icons/JavaScript.svg';
+import reactIcon from './assets/icons/React.svg';
+import flaskIcon from './assets/icons/Flask.svg';
+import sqliteIcon from './assets/icons/Sqlite.svg';
+import nodeIcon from './assets/icons/Node.svg';
+import HTMLIcon from './assets/icons/HTML.svg';
+import tkinterIcon from './assets/icons/Tkinter.png';
+import expoIcon from './assets/icons/Expo.svg';
+import SQLIcon from './assets/icons/SQL.svg';
+import bunIcon from './assets/icons/Bun.svg';
+import viteIcon from './assets/icons/Vite.svg';
 
 export const icons = {
   Java: javaIcon,
