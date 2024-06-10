53f7b12d0f6d8c26d40f3f6b2bff5af3b24cee6d
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-06-10 13:41:09 +0100

    adjustments to project displays

--- src/Components/Displays/ProjectDisplay.jsx
+++ src/Components/Displays/ProjectDisplay.jsx
@@ -1,6 +1,25 @@
 import '../../App.css';
 import { icons } from '../../App';
+import weatherWeb1 from '../../assets/softwareScreenshots/weather web 1.png';
+import weatherWeb2 from '../../assets/softwareScreenshots/weather web 2.png';
+import hike5 from '../../assets/softwareScreenshots/Hike 5.png';
+import hike7 from '../../assets/softwareScreenshots/Hike 7.png';
+import todo1 from '../../assets/softwareScreenshots/todo 1.png';
+import todo2 from '../../assets/softwareScreenshots/todo 2.png';
+import weatherChat1 from '../../assets/softwareScreenshots/Weather Chat 1.png';
+import weatherChat2 from '../../assets/softwareScreenshots/Weather Chat 2.png';
+
 export default function ProjectDisplay({ project }) {
+  const images = {
+    'weather web 1': weatherWeb1,
+    'weather web 2': weatherWeb2,
+    'hike 5': hike5,
+    'hike 7': hike7,
+    'todo 1': todo1,
+    'todo 2': todo2,
+    'weather chat 1': weatherChat1,
+    'weather chat 2': weatherChat2,
+  };
   // text sizing needs adjusting
   const LinkGithub = () => {
     const link = project.github;
@@ -11,8 +30,7 @@ export default function ProjectDisplay({ project }) {
             <a
               style={{ marginInline: 10 }}
               key={index}
-              href={item}
-            >
+              href={item}>
               Github
             </a>
           ))}
@@ -28,6 +46,7 @@ export default function ProjectDisplay({ project }) {
     } else {
       return (
         <div>
+          <p>Links:</p>
           <a href={link}>Github</a>
         </div>
       );
@@ -37,10 +56,20 @@ export default function ProjectDisplay({ project }) {
     <>
       <div id='projectDisplay'>
         <h2>{project.title}</h2>
+
+        {project.images.map((item, index) => (
+          <img
+            src={images[item]}
+            key={index}
+            id='projectScreenshot'
+            loading='lazy'
+          />
+        ))}
+
         <div id='descContainer'>
-          <p style={{ maxWidth: '50%' }}>{project.description}</p>
+          <p>{project.description}</p>
         </div>
-        <p>Links:</p>
+
         <LinkGithub />
         <p>Uses:</p>
         <div id='iconDisplay' /**maybe add glow on hover */>
